e65a5f18c9446630928541a1578561099c3b2179
dm <D.alavaro@gmail.com>
2023-10-10 16:58:11 +0300

    add menu

--- fallout_shelter/urls.py
+++ fallout_shelter/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls.static import static
 from django.urls import path
 
-from fallout_shelter.views import index, categories, about
+from fallout_shelter.views import index, contact, about
 from shop import settings
 
 urlpatterns = [
     path('', index, name='home'),
-    path('category/<int:pk>/', categories, name='category'),
-    path('about', about,  name='about'),
+    path('about/', about,  name='about'),
+    path('contact/', contact, name='contact')
 ]
 
 if settings.DEBUG:
--- fallout_shelter/views.py
+++ fallout_shelter/views.py
@@ -3,21 +3,38 @@ from django.shortcuts import render
 
 from fallout_shelter.models import Product
 
-menu = ["О сайте", "Контакты"]
+menu = [
+    {'title': 'О сайте', 'url_name': 'about'},
+    {'title': 'Контакты', 'url_name': 'contact'},
+]
 
 def index(request):
     context = {
-        'object_list': Product.objects.all()[0:3],
+        'object_list': Product.objects.all(),
         'title': 'SkystoreDM',
         'menu': menu,
     }
-    return render(request, 'fallout_shelter/index.html', context)
+    return render(request, 'fallout_shelter/index.html', context=context)
 
-def categories(request, pk):
-    return HttpResponse(f'Category: {pk}')
 
 def about(request):
-    return render(request, 'fallout_shelter/about.html', {'title': 'О сайте'})
+    context = {
+        'object_list': Product.objects.all(),
+        'title': 'О сайте',
+        'menu': menu,
+    }
+    return render(request, 'fallout_shelter/about.html', context=context)
+
+
+def contact(request):
+    context = {
+        'object_list': Product.objects.all(),
+        'title': 'Контактная информация',
+        'menu': menu,
+    }
+    return render(request, 'fallout_shelter/contact.html', context=context)
+
 
 def page_not_found(request, exception=None):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
